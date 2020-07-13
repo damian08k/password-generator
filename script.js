37a4d46c6f0d0daad2d611a82196e5920d095b21
damian08k <damiankierzkowski08@wp.pl>
2020-07-13 19:08:24 +0200

    correct checking input function, add return value

--- script.js
+++ script.js
@@ -3,26 +3,41 @@ const inputCharacters = document.querySelector(".generator-left__input--characte
 const inputSpecials = document.querySelectorAll(".generator-left__input--special");
 const labelsSpecials = document.querySelectorAll(".generator-left__label--special");
 
+const passwordSigns = {
+    smallLetters: ["abcdefghijklmnopqrstuvwxyz"],
+    bigLetters: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    specialSigns: ["!@#$%^&*()<>?=+"]
+}
 
 const checkInputsValues = () => {
+    let counter = 0;
+    const maxCounterValue = 5;
+
     if (inputCharacters.value === "") {
         alert("Number of characters area cannot be empty!");
     } else if (inputCharacters.value < 8 || inputCharacters.value > 14) {
         alert("You wrote too many number of characters. It should be 8 - 14");
+    } else {
+        counter++;
     }
 
     inputSpecials.forEach((input, index) => {
         if (input.value === "") {
             alert(`${labelsSpecials[index].innerText.slice(0, -1)} cannot be empty!`);
-        }
-        if (input.value < 1 || input.value > 10) {
+        } else if (input.value < 1 || input.value > 10) {
             alert(`You wrote too many ${labelsSpecials[index].innerText.slice(0, -1).toLowerCase()}.`);
+        } else {
+            counter++;
         }
     })
+    return counter === maxCounterValue ? true : false;
 }
 
 const createPassword = () => {
-    checkInputsValues();
+    const canCreate = checkInputsValues();
+    if (canCreate) {
+        // ...
+    }
 }
 
 passwordBtn.addEventListener("click", createPassword);
