036c4525be57cac70fd209d84ff495e2e378d4a5
damian08k <damiankierzkowski08@wp.pl>
2020-07-21 18:31:09 +0200

    Add func which create password

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const inputSpecials = document.querySelectorAll(".generator-left__input--special
 const labelsSpecials = document.querySelectorAll(".generator-left__label--special");
 
 const passwordSigns = {
-    smallLetters: ["abcdefghijklmnopqrstuvwxyz"],
-    bigLetters: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
-    specialSigns: ["!@#$%^&*()<>?=+"]
+    smallLetters: "abcdefghijklmnopqrstuvwxyz",
+    bigLetters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    digitals: "1234567890",
+    specialSigns: "!@#$%^&*()<>?=+",
 }
+
+let password = "";
+let inputPosition = 0;
 
 const checkInputsValues = (numChars) => {
     let counter = 0;
@@ -48,6 +52,34 @@ const clearInputsValues = () => {
     inputSpecials.forEach(input => input.value = "");
 }
 
+const drawingSigns = (value) => {
+    let randomNumber = 0;
+
+    for (let i = 1; i <= value; i++) {
+        switch (inputPosition) {
+            case 0:
+                randomNumber = Math.floor(Math.random() * passwordSigns.smallLetters.length + 1)
+                password += passwordSigns.smallLetters[randomNumber];
+                break;
+            case 1:
+                randomNumber = Math.floor(Math.random() * passwordSigns.bigLetters.length + 1)
+                password += passwordSigns.bigLetters[randomNumber];
+                break;
+            case 2:
+                randomNumber = Math.floor(Math.random() * passwordSigns.digitals.length + 1)
+                password += passwordSigns.digitals[randomNumber];
+                break;
+            case 3:
+                randomNumber = Math.floor(Math.random() * passwordSigns.specialSigns.length + 1)
+                password += passwordSigns.specialSigns[randomNumber];
+                break;
+        }
+
+    }
+    inputPosition++;
+    return password;
+}
+
 const createPassword = () => {
     let numChars = inputCharacters.value;
     let smallLet = inputSpecials[0].value;
@@ -59,7 +91,14 @@ const createPassword = () => {
     const isCorrect = checkIsCorrectValues(numChars, smallLet, bigLet, digitals, specSigns);
 
     if (canCreate && isCorrect) {
-        // 
+
+        drawingSigns(smallLet);
+        drawingSigns(bigLet);
+        drawingSigns(digitals);
+        drawingSigns(specSigns);
+
+        console.log(password, inputPosition);
+
     }
 
     clearInputsValues();
